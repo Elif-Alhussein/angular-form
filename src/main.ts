bff57599a94a87d8a2b39d3a4a0871d5e8affc60
Elif Alhussein <180541610@firat.edu.tr>
2025-04-25 14:26:05 +0300

    Update contact form with new changes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { usePreset } from '@primeng/themes';
+import MyPreset from './theme/MyPresets';
+
+usePreset(MyPreset);
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
