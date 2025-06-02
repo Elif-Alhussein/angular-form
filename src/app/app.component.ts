b2f01662de2a5b9c588ddcd15811605c36818753
Elif Alhussein <180541610@firat.edu.tr>
2025-06-02 11:43:17 +0300

    Edit Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,77 +1,37 @@
 import { Component } from '@angular/core';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { PassengerFormComponent } from './passenger-form/passenger-form.component';
 import {
-  FormBuilder,
   FormGroup,
-  Validators,
-  FormArray,
-  AbstractControl,
+  FormBuilder,
   FormControl,
+  FormArray,
+  Validators,
+  AbstractControl,
+  ReactiveFormsModule,
 } from '@angular/forms';
-import { ButtonModule } from 'primeng/button';
-import { FormsModule } from '@angular/forms';
-import { SelectModule } from 'primeng/select';
-import { CheckboxModule } from 'primeng/checkbox';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
-import { InputTextModule } from 'primeng/inputtext';
-import { RadioButtonModule } from 'primeng/radiobutton';
-import { DatePickerModule } from 'primeng/datepicker';
-import { MessageModule } from 'primeng/message';
-import { CalendarModule } from 'primeng/calendar';
+import { ButtonModule } from 'primeng/button';
+
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
+    ContactFormComponent,
+    PassengerFormComponent,
+    ReactiveFormsModule,
     CommonModule,
     ButtonModule,
-    FormsModule,
-    SelectModule,
-    CheckboxModule,
-    ReactiveFormsModule,
-    InputTextModule,
-    RadioButtonModule,
-    DatePickerModule,
-    MessageModule,
-    CalendarModule,
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   contactForm: FormGroup;
-  passengersForm!: FormGroup;
+  passengerForm!: FormGroup;
   submitted = false;
-  passengersSubmitted = false;
   successMessage = '';
-
-  passengerData = [
-    { id: '9049c959-cdfd-433c-ad4d-cf86c935745b', type: 'Adult (12+)' },
-    { id: '1b41af93-acf7-46c6-81ff-1c75e9a43be4', type: 'Adult (12+)' },
-    { id: 'b9d3e22d-4240-4d9b-8909-43122f0cac0f', type: 'Adult (12+)' },
-  ];
-
-  codes = [
-    { label: '+90', value: '+TR' },
-    { label: '+1', value: 'USA' },
-    { label: '+44', value: '+UK' },
-    { label: '+61', value: '+AS' },
-    { label: '+81', value: '+JP' },
-  ];
-
-  countries = [
-    { label: 'Turkey', value: 'Turkey' },
-    { label: 'United States of America', value: 'USA' },
-    { label: 'United Kingdom', value: 'UK' },
-    { label: 'Australia', value: 'Australia' },
-    { label: 'Japan', value: 'Japan' },
-  ];
-
-  nationalities = [
-    { label: 'Netherlands', value: 'Netherlands' },
-    { label: 'Turkey', value: 'Turkey' },
-    { label: 'United States', value: 'USA' },
-    { label: 'United Kingdom', value: 'UK' },
-  ];
+  passengersSubmitted = false;
 
   constructor(private fb: FormBuilder) {
     this.contactForm = this.fb.group(
@@ -107,7 +67,6 @@ export class AppComponent {
       },
       { validators: this.emailMatchValidator }
     );
-
     this.initializePassengerForms();
   }
 
@@ -121,26 +80,6 @@ export class AppComponent {
       return { emailMismatch: true };
     }
     return null;
-  }
-
-  initializePassengerForms() {
-    const passengerArray = this.passengerData.map(() =>
-      this.fb.group({
-        gender: ['', Validators.required],
-        name: ['', [Validators.required, Validators.minLength(2)]],
-        surname: ['', [Validators.required, Validators.minLength(2)]],
-        nationality: ['Netherlands', Validators.required],
-        birthDate: ['', Validators.required],
-      })
-    );
-
-    this.passengersForm = this.fb.group({
-      passengers: this.fb.array(passengerArray),
-    });
-  }
-
-  get passengerForms(): FormArray {
-    return this.passengersForm.get('passengers') as FormArray;
   }
 
   onSubmit() {
@@ -168,8 +107,8 @@ export class AppComponent {
 
   onSubmitPassengers() {
     this.passengersSubmitted = true;
-    if (this.passengersForm.valid) {
-      console.log('Passenger data:', this.passengersForm.value);
+    if (this.passengerForm.valid) {
+      console.log('Passenger data:', this.passengerForm.value);
       this.successMessage = 'Passenger Information Successfully Saved!';
 
       setTimeout(() => {
@@ -179,20 +118,13 @@ export class AppComponent {
       }, 3000);
     } else {
       console.log('Passengers Form is invalid!');
-      const passengersArray = this.passengersForm.get(
-        'passengers'
-      ) as FormArray;
+      const passengersArray = this.passengerForm.get('passengers') as FormArray;
       passengersArray.controls.forEach((control, index) => {
         if (control.invalid) {
           console.log(`Passenger ${index + 1} has errors:`, control.errors);
         }
       });
     }
-  }
-
-  onPhoneInput(event: any) {
-    const value = event.target.value;
-    event.target.value = value.replace(/[^0-9]/g, '');
   }
 
   hasError(
@@ -203,17 +135,16 @@ export class AppComponent {
   ): boolean {
     try {
       let control: AbstractControl | null;
-
-      if (index !== undefined && this.passengerForms) {
+      if (formGroup === this.contactForm) {
+        control = this.contactForm.get(controlName);
+        return Boolean(
+          control?.hasError(errorType) && (control?.touched || this.submitted)
+        );
+      } else if (index !== undefined && this.passengerForms) {
         control = this.passengerForms.at(index).get(controlName);
         return Boolean(
           control?.hasError(errorType) &&
             (control?.touched || this.passengersSubmitted)
-        );
-      } else if (formGroup === this.contactForm) {
-        control = this.contactForm.get(controlName);
-        return Boolean(
-          control?.hasError(errorType) && (control?.touched || this.submitted)
         );
       }
     } catch (error) {
@@ -222,9 +153,34 @@ export class AppComponent {
 
     return false;
   }
+
   getGenderControl(index: number): FormControl {
-    return (this.passengersForm.get('passengers') as FormArray)
+    return (this.passengerForm.get('passengers') as FormArray)
       .at(index)
       .get('gender') as FormControl;
+  }
+
+initializePassengerForms() {
+  const passengerCount = 3; 
+  const passengerArray = Array(passengerCount).fill(null).map(() =>
+    this.fb.group({
+      gender: ['', Validators.required],
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      surname: ['', [Validators.required, Validators.minLength(2)]],
+      nationality: ['Netherlands', Validators.required],
+      birthDate: ['', Validators.required],
+    })
+  );
+
+  this.passengerForm = this.fb.group({
+    passengers: this.fb.array(passengerArray),
+  });
+}
+
+  get passengerForms(): FormArray {
+    return this.passengerForm.get('passengers') as FormArray;
+  }
+  getPassengerFormGroup(index: number): FormGroup {
+    return this.passengerForms.at(index) as FormGroup;
   }
 }
--- src/app/passenger-form/passenger-form.component.ts
+++ src/app/passenger-form/passenger-form.component.ts
@@ -1,46 +1,31 @@
-import { Component } from '@angular/core';
-import {
-  FormBuilder,
-  FormGroup,
-  Validators,
-  FormArray,
-  AbstractControl,
-  FormControl,
-} from '@angular/forms';
-import { ButtonModule } from 'primeng/button';
-import { FormsModule } from '@angular/forms';
-import { SelectModule } from 'primeng/select';
-import { CheckboxModule } from 'primeng/checkbox';
+import { Component, Input } from '@angular/core';
+import { FormGroup, FormControl, AbstractControl } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import { InputTextModule } from 'primeng/inputtext';
 import { RadioButtonModule } from 'primeng/radiobutton';
-import { DatePickerModule } from 'primeng/datepicker';
-import { MessageModule } from 'primeng/message';
+import { InputTextModule } from 'primeng/inputtext';
+import { DropdownModule } from 'primeng/dropdown';
 import { CalendarModule } from 'primeng/calendar';
+import { DatePickerModule } from 'primeng/datepicker';
 
 @Component({
   selector: 'app-passenger-form',
+  standalone: true,
   imports: [
     CommonModule,
-    ButtonModule,
-    FormsModule,
-    SelectModule,
-    CheckboxModule,
     ReactiveFormsModule,
-    InputTextModule,
     RadioButtonModule,
-    DatePickerModule,
-    MessageModule,
+    InputTextModule,
+    DropdownModule,
     CalendarModule,
+    DatePickerModule,
   ],
   templateUrl: './passenger-form.component.html',
-  styleUrl: './passenger-form.component.css',
 })
 export class PassengerFormComponent {
-  passengersForm!: FormGroup;
-  passengersSubmitted = false;
-  successMessage = '';
+  @Input() passengerForm!: AbstractControl;
+  @Input() index!: number;
+  @Input() submitted: boolean = false;
 
   passengerData = [
     { id: '9049c959-cdfd-433c-ad4d-cf86c935745b', type: 'Adult (12+)' },
@@ -53,80 +38,22 @@ export class PassengerFormComponent {
     { label: 'United States', value: 'USA' },
     { label: 'United Kingdom', value: 'UK' },
   ];
-
-  constructor(private fb: FormBuilder) {
-    this.initializePassengerForms();
+  get formGroup(): FormGroup {
+    return this.passengerForm as FormGroup;
   }
 
-  initializePassengerForms() {
-    const passengerArray = this.passengerData.map(() =>
-      this.fb.group({
-        gender: ['', Validators.required],
-        name: ['', [Validators.required, Validators.minLength(2)]],
-        surname: ['', [Validators.required, Validators.minLength(2)]],
-        nationality: ['Netherlands', Validators.required],
-        birthDate: ['', Validators.required],
-      })
+  get passengerType(): string {
+    return this.passengerData[this.index]?.type || 'Passenger';
+  }
+
+  hasError(controlName: string, errorType: string): boolean {
+    const control = this.formGroup.get(controlName);
+    return (
+      !!control?.hasError(errorType) && (control?.touched || this.submitted)
     );
-
-    this.passengersForm = this.fb.group({
-      passengers: this.fb.array(passengerArray),
-    });
   }
 
-  get passengerForms(): FormArray {
-    return this.passengersForm.get('passengers') as FormArray;
-  }
-
-  onSubmitPassengers() {
-    this.passengersSubmitted = true;
-    if (this.passengersForm.valid) {
-      console.log('Passenger data:', this.passengersForm.value);
-      this.successMessage = 'Passenger Information Successfully Saved!';
-
-      setTimeout(() => {
-        this.initializePassengerForms();
-        this.passengersSubmitted = false;
-        this.successMessage = '';
-      }, 3000);
-    } else {
-      console.log('Passengers Form is invalid!');
-      const passengersArray = this.passengersForm.get(
-        'passengers'
-      ) as FormArray;
-      passengersArray.controls.forEach((control, index) => {
-        if (control.invalid) {
-          console.log(`Passenger ${index + 1} has errors:`, control.errors);
-        }
-      });
-    }
-  }
-
-  hasError(
-    formGroup: any,
-    controlName: string,
-    errorType: string,
-    index?: number
-  ): boolean {
-    try {
-      let control: AbstractControl | null;
-
-      if (index !== undefined && this.passengerForms) {
-        control = this.passengerForms.at(index).get(controlName);
-        return Boolean(
-          control?.hasError(errorType) &&
-            (control?.touched || this.passengersSubmitted)
-        );
-      }
-    } catch (error) {
-      console.log('Error in hasError function:', error);
-    }
-
-    return false;
-  }
-  getGenderControl(index: number): FormControl {
-    return (this.passengersForm.get('passengers') as FormArray)
-      .at(index)
-      .get('gender') as FormControl;
+  getGenderControl(): FormControl {
+    return this.formGroup.get('gender') as FormControl;
   }
 }
